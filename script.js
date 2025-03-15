039008b17a32157c97b637a82459f11a0e03a89b
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-03-15 21:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let timerdisplay = document.getElementById("timer");
+let currentDate = new date();
+set Interval(
+	function(){
+		currentDate = new date();
+		timerdisplay.innerHTML = currentDate.toLocaleString();
+	
+	},1000
+)
